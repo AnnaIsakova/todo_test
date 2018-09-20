c734b1bb769ad3304ab4ab885115ffcf0b6232c7
AnnaIsakova <tamgdepesok666@gmail.com>
2018-09-20 21:51:17 +0300

    refactored rest repo:

--- src/main/java/CrudRepository.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface CrudRepository<T> {
-
-    void save(T item, String url);
-    List<T> getAll(String url);
-    void updateItem(T item, String url);
-    void deleteItem(long id, String url);
-}
