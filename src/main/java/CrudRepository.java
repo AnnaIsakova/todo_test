94f17eea203002bdd5a9f3e7c13c2d7c34e20294
AnnaIsakova <tamgdepesok666@gmail.com>
2018-09-20 20:50:36 +0300

    refactored api

--- src/main/java/CrudRepository.java
+++ src/main/java/CrudRepository.java
@@ -2,8 +2,8 @@ import java.util.List;
 
 public interface CrudRepository<T> {
 
-    void save(T item);
-    List<T> getAll();
-    void updateItem(T item);
-    void deleteItem(long id);
+    void save(T item, String url);
+    List<T> getAll(String url);
+    void updateItem(T item, String url);
+    void deleteItem(long id, String url);
 }
--- src/main/java/TodoItemRepository.java
+++ src/main/java/TodoItemRepository.java
@@ -2,19 +2,19 @@ import java.util.List;
 
 public class TodoItemRepository implements CrudRepository<TodoItem> {
 
-    public void save(TodoItem item) {
+    public void save(TodoItem item, String url) {
 
     }
 
-    public List<TodoItem> getAll() {
+    public List<TodoItem> getAll(String url) {
         return null;
     }
 
-    public void updateItem(TodoItem item) {
+    public void updateItem(TodoItem item, String url) {
 
     }
 
-    public void deleteItem(long id) {
+    public void deleteItem(long id, String url) {
 
     }
 }
