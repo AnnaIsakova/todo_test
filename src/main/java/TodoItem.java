d13883e7350f6f95b7a8bf38fb806de1b8d922b4
AnnaIsakova <tamgdepesok666@gmail.com>
2018-09-20 21:52:59 +0300

    added status field + annotation for deserialization

--- src/main/java/TodoItem.java
+++ src/main/java/TodoItem.java
@@ -1,9 +1,14 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TodoItem {
     private long id;
     private String description;
+    private Status status;
 }
