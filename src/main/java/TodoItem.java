193246db35dcf15386bdae60571d229529b5a3ec
AnnaIsakova <tamgdepesok666@gmail.com>
2018-09-20 23:50:06 +0300

    changed id column name

--- src/main/java/TodoItem.java
+++ src/main/java/TodoItem.java
@@ -1,14 +1,17 @@
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TodoItem {
-    private long id;
+    private String objectId;
     private String description;
     private Status status;
+
+    public TodoItem(String description, Status status) {
+        this.description = description;
+        this.status = status;
+    }
 }
