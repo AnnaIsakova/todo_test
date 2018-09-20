c44138514a137d12cb7ec352e78a8dd8dbf04ce5
AnnaIsakova <tamgdepesok666@gmail.com>
2018-09-20 20:26:56 +0300

    created basic todo item

--- src/main/java/TodoItem.java
+++ src/main/java/TodoItem.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class TodoItem {
+    private long id;
+    private String description;
 }
