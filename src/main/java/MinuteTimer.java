cc4cee0c92091c1b0cd3e800c30734225615ee07
AnnaIsakova <tamgdepesok666@gmail.com>
2018-09-21 01:06:19 +0300

    added timer

---
+++ src/main/java/MinuteTimer.java
@@ -0,0 +1,28 @@
+import com.backendless.servercode.annotation.BackendlessTimer;
+import com.backendless.servercode.extension.TimerExtender;
+import com.mashape.unirest.http.Unirest;
+
+import java.util.List;
+
+@BackendlessTimer("{'startDate':1537478569,'frequency':{'schedule':'custom','repeat':{'every':60}},'timername':'Minute'}")
+public class MinuteTimer extends TimerExtender {
+    CrudRepository<TodoItem> repository = new TodoItemRepository();
+    @Override
+    public void execute() {
+        System.out.println("FDFDFD");
+        try {
+            List<TodoItem> items = repository.getAll();
+            items.forEach(item->{
+                if(item.getStatus().equals(Status.COMPLETED)){
+                    this.sendDeleteRequest(item.getObjectId());
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendDeleteRequest(String itemId){
+        Unirest.delete(UrlBuilder.getUrlWithId(itemId));
+    }
+}
