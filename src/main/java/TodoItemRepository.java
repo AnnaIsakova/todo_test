8e33c5ba2ee50fbba54cf1652abdd9bdde81ff4c
AnnaIsakova <tamgdepesok666@gmail.com>
2018-09-20 22:04:17 +0300

    created basic implementation for todo repo

--- src/main/java/TodoItemRepository.java
+++ src/main/java/TodoItemRepository.java
@@ -1,20 +1,45 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class TodoItemRepository implements CrudRepository<TodoItem> {
+public class TodoItemRepository implements RestRepository<TodoItem> {
 
-    public void save(TodoItem item, String url) {
-
+    public TodoItem save(TodoItem item, String url) throws UnirestException {
+        HttpResponse<TodoItem> response = Unirest.post(url)
+                .header("Content-Type", "application/json")
+                .body(item)
+                .asObject(TodoItem.class);
+        return response.getBody();
     }
 
-    public List<TodoItem> getAll(String url) {
-        return null;
+    public List<TodoItem> getAll(String url) throws UnirestException {
+        HttpResponse<TodoItem[]> response = Unirest
+                .get(url)
+                .asObject(TodoItem[].class);
+        return new ArrayList<TodoItem>(Arrays.asList(response.getBody()));
     }
 
-    public void updateItem(TodoItem item, String url) {
-
+    public TodoItem updateItem(TodoItem item, String url) throws UnirestException {
+        HttpResponse<TodoItem> response = Unirest.put(url)
+                .header("Content-Type", "application/json")
+                .body(item)
+                .asObject(TodoItem.class);
+        return response.getBody();
     }
 
-    public void deleteItem(long id, String url) {
-
+    public void deleteItem(long id, String url) throws UnirestException {
+        HttpResponse<TodoItem> getResponse = Unirest
+                .get(url)
+                .asObject(TodoItem.class);
+        TodoItem itemToUpdate = getResponse.getBody();
+        itemToUpdate.setStatus(Status.COMPLETED);
+        Unirest.put(url)
+            .header("Content-Type", "application/json")
+            .body(itemToUpdate)
+            .asObject(TodoItem.class);
     }
 }
