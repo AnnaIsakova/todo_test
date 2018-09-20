62aa0f78d83677aba19298b0aea511b826530a40
AnnaIsakova <tamgdepesok666@gmail.com>
2018-09-20 20:35:30 +0300

    created todo item repo stub

---
+++ src/main/java/TodoItemRepository.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class TodoItemRepository implements CrudRepository<TodoItem> {
+
+    public void save(TodoItem item) {
+
+    }
+
+    public List<TodoItem> getAll() {
+        return null;
+    }
+
+    public void updateItem(TodoItem item) {
+
+    }
+
+    public void deleteItem(long id) {
+
+    }
+}
