ddc68b9fd31a9a928c15ca7ca30c32ae3aa52d9d
AnnaIsakova <tamgdepesok666@gmail.com>
2018-09-20 20:47:44 +0300

    configured unirest object mapper

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,37 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mashape.unirest.http.ObjectMapper;
+import com.mashape.unirest.http.Unirest;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        setUnirestObjectMapper();
 
+    }
+
+    public static void setUnirestObjectMapper() {
+        Unirest.setObjectMapper(new ObjectMapper() {
+            com.fasterxml.jackson.databind.ObjectMapper mapper
+                    = new com.fasterxml.jackson.databind.ObjectMapper();
+
+            public String writeValue(Object value) {
+                try {
+                    return mapper.writeValueAsString(value);
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            public <T> T readValue(String value, Class<T> valueType) {
+                try {
+                    return mapper.readValue(value, valueType);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        });
     }
 }
